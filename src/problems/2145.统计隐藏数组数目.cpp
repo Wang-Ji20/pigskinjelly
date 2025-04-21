8080c970846f32e8115014ba12da8883278174ed
hortensia <824466875@qq.com>
2025-04-21 15:01:35 +0800

    daily 4/21

---
+++ src/problems/2145.统计隐藏数组数目.cpp
@@ -0,0 +1,106 @@
+#include "lc_common.hh"
+
+/*
+ * @lc app=leetcode.cn id=2145 lang=cpp
+ *
+ * [2145] 统计隐藏数组数目
+ *
+ * https://leetcode.cn/problems/count-the-hidden-sequences/description/
+ *
+ * algorithms
+ * Medium (37.38%)
+ * Likes:    41
+ * Dislikes: 0
+ * Total Accepted:    15.2K
+ * Total Submissions: 34K
+ * Testcase Example:  '[1,-3,4]\n1\n6'
+ *
+ * 给你一个下标从 0 开始且长度为 n 的整数数组 differences ，它表示一个长度为 n +
+ * 1 的 隐藏 数组 相邻 元素之间的 差值
+ * 。更正式的表述为：我们将隐藏数组记作 hidden ，那么 differences[i] = hidden[i
+ * + 1] - hidden[i] 。
+ *
+ * 同时给你两个整数 lower 和 upper ，它们表示隐藏数组中所有数字的值都在
+ * 闭 区间 [lower, upper] 之间。
+ *
+ *
+ * 比方说，differences = [1, -3, 4] ，lower = 1 ，upper =
+ * 6 ，那么隐藏数组是一个长度为 4 且所有值都在 1 和 6 （包含两者）之间的数组。
+ *
+ *
+ * [3, 4, 1, 5] 和 [4, 5, 2, 6] 都是符合要求的隐藏数组。
+ * [5, 6, 3, 7] 不符合要求，因为它包含大于 6 的元素。
+ * [1, 2, 3, 4] 不符合要求，因为相邻元素的差值不符合给定数据。
+ *
+ *
+ *
+ *
+ * 请你返回 符合 要求的隐藏数组的数目。如果没有符合要求的隐藏数组，请返回 0 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：differences = [1,-3,4], lower = 1, upper = 6
+ * 输出：2
+ * 解释：符合要求的隐藏数组为：
+ * - [3, 4, 1, 5]
+ * - [4, 5, 2, 6]
+ * 所以返回 2 。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：differences = [3,-4,5,1,-2], lower = -4, upper = 5
+ * 输出：4
+ * 解释：符合要求的隐藏数组为：
+ * - [-3, 0, -4, 1, 2, 0]
+ * - [-2, 1, -3, 2, 3, 1]
+ * - [-1, 2, -2, 3, 4, 2]
+ * - [0, 3, -1, 4, 5, 3]
+ * 所以返回 4 。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：differences = [4,-7,2], lower = 3, upper = 6
+ * 输出：0
+ * 解释：没有符合要求的隐藏数组，所以返回 0 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == differences.length
+ * 1 <= n <= 10^5
+ * -10^5 <= differences[i] <= 10^5
+ * -10^5 <= lower <= upper <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+  int
+  numberOfArrays(vector<int> &differences, int lower, int upper) {
+    long long min_differences = 0;
+    long long max_differences = 0;
+
+    long long running_differences = 0;
+    for (int d : differences) {
+      running_differences += d;
+      min_differences = min(min_differences, running_differences);
+      max_differences = max(max_differences, running_differences);
+    }
+
+    if (max_differences - min_differences > upper - lower) {
+      return 0;
+    }
+
+    return (upper - lower + 1) - (max_differences - min_differences);
+  }
+};
+// @lc code=end
